7cc864152f5754e5f5e59e58618f8197dd00a9fc
Cooper Edmunds <cooper6789@gmail.com>
2018-12-06 17:42:44 -0500

    Fix bug

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -96,7 +96,7 @@ public class HuffProcessor {
 				if (bits == 0) current = current.myLeft;
 				else current = current.myRight;
 				
-				if (current != null && current.myValue != 0) {
+				if (current != null & current.myLeft == null && current.myRight == null) {
 					if (current.myValue == PSEUDO_EOF) {
 						break;   // out of loop
 					}
